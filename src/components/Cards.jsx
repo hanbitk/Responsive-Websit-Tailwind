cad3bde605c9132adfe6e8447a08f878db8ad54f
Hanbit Kang <hanbitkang95@gmail.com>
2023-05-23 17:15:32 +0900

    eslint, prettier and tailwind config

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Single from "../assests/single.png";
-import Double from "../assests/double.png";
-import Triple from "../assests/triple.png";
+import React from 'react';
+import Single from '../assests/single.png';
+import Double from '../assests/double.png';
+import Triple from '../assests/triple.png';
 
 function Cards() {
   return (
@@ -9,7 +9,11 @@ function Cards() {
       {/* Container */}
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
         <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
-          <img className="w-20 mx-auto mt-[-3rem] bg-white" src={Single} alt="/" />
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={Single}
+            alt="/"
+          />
           <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
           <p className="text-center text-4xl font-bold">$149</p>
           <div className="text-center font-medium">
@@ -17,11 +21,17 @@ function Cards() {
             <p className="py-2 border-b mx-8">1 Granted User</p>
             <p className="py-2 border-b mx-8">Send up to 2GB</p>
           </div>
-          <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3" >Start Trial</button>
+          <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
+            Start Trial
+          </button>
         </div>
 
         <div className="w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
-          <img className="w-20 mx-auto mt-[-3rem] bg-transparent" src={Double} alt="/" />
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-transparent"
+            src={Double}
+            alt="/"
+          />
           <h2 className="text-2xl font-bold text-center py-8">Double User</h2>
           <p className="text-center text-4xl font-bold">$149</p>
           <div className="text-center font-medium">
@@ -29,11 +39,17 @@ function Cards() {
             <p className="py-2 border-b mx-8">1 Granted User</p>
             <p className="py-2 border-b mx-8">Send up to 2GB</p>
           </div>
-          <button className="bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3" >Start Trial</button>
+          <button className="bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
+            Start Trial
+          </button>
         </div>
 
         <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
-          <img className="w-20 mx-auto mt-[-3rem] bg-white" src={Triple} alt="/" />
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={Triple}
+            alt="/"
+          />
           <h2 className="text-2xl font-bold text-center py-8">Triple User</h2>
           <p className="text-center text-4xl font-bold">$149</p>
           <div className="text-center font-medium">
@@ -41,7 +57,9 @@ function Cards() {
             <p className="py-2 border-b mx-8">1 Granted User</p>
             <p className="py-2 border-b mx-8">Send up to 2GB</p>
           </div>
-          <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3" >Start Trial</button>
+          <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
+            Start Trial
+          </button>
         </div>
       </div>
     </div>
